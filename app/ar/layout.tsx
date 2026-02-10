4e2801c9926ae112e6a69ef37d1cabae888855fa
Anothays <jeremy.snnk@gmail.com>
2026-02-10 19:39:27 +0100

    delete aFrame components

--- app/ar/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function ArLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <>{children}</>;
-}
--- app/ar/page.tsx
+++
@@ -1,9 +0,0 @@
-import ArPageClient from "./ArPageClient";
-
-export default function ArPage() {
-  return (
-    <div className="fixed inset-0 w-screen h-screen overflow-hidden">
-      <ArPageClient />
-    </div>
-  );
-}
