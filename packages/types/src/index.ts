30083988ac9112da8e25f0a5b3def093827faf08
Anothays <jeremy.snnk@gmail.com>
2026-03-10 21:14:05 +0100

    feat: add dto

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,2 +1,5 @@
+export * from "./common/paginated-result";
 export * from "./products/dto/create-product.request";
 export * from "./products/interfaces/product.interface";
+export * from "./users/dto/create-user.dto";
+export * from "./users/dto/update-user.dto";
