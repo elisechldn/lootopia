713a52f7c3b246388bfea9cdbd0c4bb538c72751
Anothays <jeremy.snnk@gmail.com>
2026-03-10 21:16:40 +0100

    add apiResponse interfaces

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,6 @@
-export * from "./common/paginated-result";
+export * from "./apiResponse/paginated-result";
 export * from "./products/dto/create-product.request";
 export * from "./products/interfaces/product.interface";
 export * from "./users/dto/create-user.dto";
 export * from "./users/dto/update-user.dto";
+
