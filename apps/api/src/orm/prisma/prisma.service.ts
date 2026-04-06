b1a35466d801951f5be980051bece4cf521bbd74
Anothays <jeremy.snnk@gmail.com>
2026-04-06 22:32:37 +0200

    refactor: add $executeRaw() method

--- apps/api/src/orm/prisma/prisma.service.ts
+++ apps/api/src/orm/prisma/prisma.service.ts
@@ -13,11 +13,53 @@ export class PrismaService implements OnModuleInit, OnModuleDestroy {
     this.prisma = new PrismaClient({ adapter });
   }
 
-  get user() { return this.prisma.user; }
-  get hunt() { return this.prisma.hunt; }
-  get step() { return this.prisma.step; }
-  get participation() { return this.prisma.participation; }
+  get user() {
+    return this.prisma.user;
+  }
 
-  async onModuleInit() { await this.prisma.$connect(); }
-  async onModuleDestroy() { await this.prisma.$disconnect(); }
+  get hunt() {
+    return this.prisma.hunt;
+  }
+
+  get step() {
+    return this.prisma.step;
+  }
+
+  get participation() {
+    return this.prisma.participation;
+  }
+
+  get progress() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.prisma.progress;
+  }
+
+  get clue() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.prisma.clue;
+  }
+
+  get clueUsage() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.prisma.clueUsage;
+  }
+
+  $queryRaw<T = unknown>(
+    query: TemplateStringsArray | Prisma.Sql,
+    ...values: unknown[]
+  ): Promise<T> {
+    return this.prisma.$queryRaw<T>(query as Prisma.Sql, ...values);
+  }
+
+  get $executeRaw() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.prisma.$executeRaw.bind(this.prisma);
+  }
+
+  async onModuleInit() {
+    await this.prisma.$connect();
+  }
+  async onModuleDestroy() {
+    await this.prisma.$disconnect();
+  }
 }
