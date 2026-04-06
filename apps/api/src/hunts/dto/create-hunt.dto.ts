81c82b6196e16de326c807a1e910f26eb0f0995e
Anothays <jeremy.snnk@gmail.com>
2026-04-06 22:59:30 +0200

    feat: change imports to @repo/types

--- apps/api/src/hunts/dto/create-hunt.dto.ts
+++ apps/api/src/hunts/dto/create-hunt.dto.ts
@@ -1,16 +1,16 @@
-import { HuntStatus } from '../../orm/prisma/generated/enums';
+import { HuntStatus } from '@repo/types';
 
 export class CreateHuntDto {
-    title: string;
-    shortDescription?: string;
-    description?: string;
-    startDate?: string;
-    endDate?: string;
-    location?: string;
-    radius?: number;
-    difficulty?: string;
-    status?: HuntStatus;
-    rewardType?: string;
-    rewardValue?: string;
-    refUser: number;
+  title: string;
+  shortDescription?: string;
+  description?: string;
+  startDate?: string;
+  endDate?: string;
+  locationLat?: number;
+  locationLon?: number;
+  radius?: number;
+  status?: HuntStatus;
+  rewardType?: string;
+  rewardValue?: string;
+  refUser: number;
 }
