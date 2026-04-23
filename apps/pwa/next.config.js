13330b63056c508aca2bc7e02132fcb73ea86b2a
Anothays <jeremy.snnk@gmail.com>
2026-04-24 00:47:26 +0200

    feat: set standalone mode and rename INTERNAL_API_URL into API_URL

--- apps/pwa/next.config.js
+++ apps/pwa/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   rewrites: async () => [
     {
       source: '/api/:path*',
-      destination: `${process.env.INTERNAL_API_URL ?? 'http://localhost:8000'}/:path*`,
+      destination: `${process.env.API_URL ?? 'http://localhost:8000'}/:path*`,
     },
   ],
 };
