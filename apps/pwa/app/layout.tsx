cfda271fa5cd597583e43c3adc16313b7bd3704d
Anothays <jeremy.snnk@gmail.com>
2026-06-09 23:59:07 +0200

    fix: fix font loading

--- apps/pwa/app/layout.tsx
+++ apps/pwa/app/layout.tsx
@@ -47,8 +47,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="antialiased">
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
